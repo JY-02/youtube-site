03aeb75600dd60fa9787e671e6dd5c49083975b8
JY-02 <124655720+JY-02@users.noreply.github.com>
2023-02-09 10:44:42 +0900

    api upload

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 
-const API_KEY=`AIzaSyCzuCIIbuYPo1QaJqeDyJw-p8cNQzVR0nQ`;
+const API_KEY=process.env.REACT_APP_API_KEY;
 
 export const videoUrl=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&regionCode=kr&key=${API_KEY}`
 
